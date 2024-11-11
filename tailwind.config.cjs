29346274a02ab8644e96c1e323f835e4c3fae492
Karla Díaz <diazaguilarkarla8@gmail.com>
2024-11-11 00:43:07 -0600

    docs:

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,24 +6,24 @@ const config = {
   theme: {
     extend: {
       colors: {
-        tapiz: '#EBF0F6',
-        primario: '#0568FD',
-        secundario: '#FAE6E9',
+        'tapiz': '#EBF0F6',
+        'primario': '#0568FD',
+        'secundario': '#FAE6E9',
         'base-3': '#F18789',
         'base-4': '#F1A662',
-        black: '#263238',
+        'black': '#263238',
         'd-grey': '#4D4D4D',
-        grey: '#717171',
+        'grey': '#717171',
         'l-grey': '#89939E',
         'grey-blue': '#ABBED1',
-        silver: '#F5F7FA',
+        'silver': '#F5F7FA',
       },
       fontFamily: {
         main: ['Montserrat', 'Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
         secondary: ['Montserrat Alternates', 'sans-serif'],
       },
       fontWeight: {
-        bold: 700,
+        bold: '700',
       },
     },
   },
@@ -32,8 +32,8 @@ const config = {
     function ({ addBase, theme }) {
       addBase({
         ':root': {
-          '--font-main': theme('fontFamily.main').join(', '),
-          '--font-secondary': theme('fontFamily.secondary').join(', '),
+          '--font-main': theme('fontFamily.main')[0] || 'sans-serif', // Usamos el primer valor del array
+          '--font-secondary': theme('fontFamily.secondary')[0] || 'sans-serif',
           '--font-bold': theme('fontWeight.bold'),
           '--tapiz': theme('colors.tapiz'),
           '--primario': theme('colors.primario'),
