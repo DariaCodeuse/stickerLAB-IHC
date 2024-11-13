b086bbfc6b4a68263c62180235d7caa751406876
Ale Cortez <alejandracastellanoscortez55@gmail.com>
2024-11-13 15:54:23 -0600

    perfiles de creadores

--- src/Home.svelte
+++ src/Home.svelte
@@ -48,13 +48,88 @@
   </div>
 
   <!-- Sección 3: Otra sección adicional -->
-  <div class="w-full max-w-3xl text-center p-6 bg-white rounded-lg shadow">
-    <h2 class="text-2xl font-semibold mb-4">Más Opciones</h2>
-    <p class="text-gray-700">Aquí puedes agregar otra sección con diferentes opciones o información adicional.</p>
+  <div class="w-full text-center p-6 bg-white rounded-lg shadow">
+    <h2 class="text-2xl font-semibold mb-4">Creadores</h2>
+    <div class="card-container flex justify-center space-x-4">
+      <div class="card">
+          <div class="icon"></div>
+          <div class="text">
+              <p class="name">Github Nombre</p>
+              <p class="price">Price</p>
+          </div>
+      </div>
+      <div class="card">
+          <div class="icon"></div>
+          <div class="text">
+              <p class="name">Github Nombre</p>
+              <p class="price">Price</p>
+          </div>
+      </div>
+      <div class="card">
+          <div class="icon"></div>
+          <div class="text">
+              <p class="name">Github Nombre</p>
+              <p class="price">Price</p>
+          </div>
+      </div>
+      <div class="card">
+          <div class="icon"></div>
+          <div class="text">
+              <p class="name">Github Nombre</p>
+              <p class="price">Price</p>
+          </div>
+      </div>
+      <div class="card">
+          <div class="icon"></div>
+          <div class="text">
+              <p class="name">Github Nombre</p>
+              <p class="price">Price</p>
+          </div>
+      </div>
   </div>
+  </div>
+ 
 
 </div>
 
 <style>
   /* Añade estilos personalizados aquí si es necesario */
+  .card-container .card {
+          display: flex;
+          width: 245px;
+          height: 80px;
+          padding: 15px 20px 15px 0px;
+          align-items: center;
+          gap: 5px;
+          flex-shrink: 0;
+          border-radius: 8px;
+          border: 1px solid var(--Grey-blue, #ABBED1);
+          background: #F3F4F6;
+      }
+
+      .card-container .icon {
+          width: 55px;
+          height: 50px;
+          flex-shrink: 0;
+          background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='55' height='50' viewBox='0 0 55 50' fill='none'%3E%3Crect x='10' width='45' height='50' rx='10' fill='%233B82F6' fill-opacity='0.5'/%3E%3C/svg%3E");
+          background-size: cover;
+          background-repeat: no-repeat;
+      }
+
+
+      .card-container .text {
+          text-align: left;
+      }
+
+      .card-container .name {
+          font-size: 12px;
+          font-weight: bold;
+          color: #333;
+
+      }
+
+      .card-container .price {
+          font-size: 10px;
+          color: #888;
+      }
 </style>
